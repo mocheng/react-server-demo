e831cdb3845935642ae8853536fecb93a120d8ee
Morgan Cheng <morgan.cheng@hulu.com>
2017-03-27 14:04:02 +0800

    repo react-server-redux build fail

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {RootElement, RootContainer, TheFold, Link, getCurrentRequestContext} from 'react-server';
 
+//Below import breaks production asset build
+import {ReduxAdapter} from 'react-server-redux';
+
 export default class HomePage {
   getElements() {
     return (
